2ae6cad47342a6b4a257d3d849655b87f62f3fdc
pengsun <sunpengde666@gmail.com>
2022-10-27 21:15:16 +0800

    operator[] overloading 1. const std::vector<object> & operator[]() const {} 2. std::vector<object> & operator[]() const {}

---
+++ mark_allen_weiss/data_structures_and_algorithm_analysis_in_cpp/chp1/sec7/p2/use_matrix.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "matrix.hpp"
+
+
+int main()
+{
+
+    std::cout << "The Data Members, Constructors, and Basic Accessors" << std::endl;
+    matrix::matrix<double> m1(3, 4);
+    matrix::matrix<double> m2(3, 4);
+    matrix::copy(m2, m1);
+
+    matrix::matrix<double> m3{1,0};
+   //  matrix::matrix<double> m4(std::move(m3));
+    std::cout << "m3 (empty matrix): " << "numrows: " << m3.numrows() << "numcols: " << m3.numcols() << std::endl;
+    std::cout <<  m1.numrows() << std::endl << m1.numcols() << std::endl;
+    
+}
