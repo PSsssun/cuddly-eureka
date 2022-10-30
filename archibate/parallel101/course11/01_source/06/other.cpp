44407ad081b306c77979c5d8a0f8484ca4646634
pengsun <sunpengde666@gmail.com>
2022-10-30 15:09:49 +0800

    use GLOB to automatically search all *.cpp *.h files and name it sources

---
+++ archibate/parallel101/course11/01_source/06/other.cpp
@@ -0,0 +1,7 @@
+#include "other.h"
+#include <cstdio>
+
+void say_hello()
+{
+    printf("say hello from other.cpp\n");
+}
---
+++ archibate/parallel101/course11/01_source/06/run.sh
@@ -0,0 +1,5 @@
+cmake -B build
+cmake --build build
+build/main
+rm -rf build
+
