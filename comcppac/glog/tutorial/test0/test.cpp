274cbe0d8a46997f49ef198f83a8641db8a2606d
Peng Sun <sunpengde666@gmail.com>
2022-11-11 12:22:21 +0800

    now terminal will print out log's infos

--- comcppac/glog/tutorial/test0/test.cpp
+++ comcppac/glog/tutorial/test0/test.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char* argv[]) {
     // Initialize Google's logging library.
-    google::InitGoogleLogging(argv[0]);
+   // google::InitGoogleLogging(argv[0]);
     int num_cookies = 6;
     LOG(INFO) << "Found" << num_cookies << " cookies";
+    VLOG_IF(1, (num_cookies > 3))
+        << "I'm printed when size is more than 3";
+    return 0;
 }
